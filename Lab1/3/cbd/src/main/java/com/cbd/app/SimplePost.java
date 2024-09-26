059b751dbf79dc7c8189d7433c7fdea1ba63e73d
Joao <joaomonteiro29@ua.pt>
2024-09-26 12:59:06 +0100

    autocomplete

--- Lab1/3/cbd/src/main/java/com/cbd/app/SimplePost.java
+++ Lab1/3/cbd/src/main/java/com/cbd/app/SimplePost.java
@@ -1,39 +1,34 @@
-package redis;
+package com.cbd.app;
+import redis.clients.jedis.Jedis; 
+import java.util.List;
+import java.util.HashMap;
+  
+public class SimplePost { 
+    public static String USERS_KEY = "users"; // Key set for users' name
+	public static String USERS_LIST = "users_list"; // List for users' name
+	public static String USERS_MAP =  "users_map"; // List for users' hash map
+  
+    public static void main(String[] args) { 
+        Jedis jedis = new Jedis(); 
+        // some users 
+        String[] users = { "Ana", "Pedro", "Maria", "Luis" }; 
+ 
+        // jedis.del(USERS_KEY); // remove if exists to avoid wrong type 
+        for (String user : users)  
+            jedis.sadd(USERS_KEY, user); 
+			jedis.lpush(USERS_LIST, user);
+			jedis.hset(USERS_MAP, user, user); 
 
-import java.util.Set;
-import redis.clients.jedis.Jedis;
- 
-public class SimplePost {
- 
-	private Jedis jedis;
-	public static String USERS = "users"; // Key set for users' name
-	
-	public SimplePost() {
-		this.jedis = new Jedis("localhost", 1234);
-	}
- 
-	public void saveUser(String username) {
-		jedis.sadd(USERS, username);
-	}
-	public Set<String> getUser() {
-		return jedis.smembers(USERS);
-	}
-	
-	public Set<String> getAllKeys() {
-		return jedis.keys("*");
-	}
- 
-	public static void main(String[] args) {
-		SimplePost board = new SimplePost();
-		// set some users
-		String[] users = { "Ana", "Pedro", "Maria", "Luis" };
-		for (String user: users) 
-			board.saveUser(user);
-		board.getAllKeys().stream().forEach(System.out::println);
-		board.getUser().stream().forEach(System.out::println);
-		
-	}
-}
+
+        jedis.smembers(USERS_KEY).forEach(System.out::println); 
+        
+        jedis.lrange(USERS_LIST, 0, -1).forEach(System.out::println);
+
+        jedis.hgetAll(USERS_MAP).forEach((key, value) -> System.out.println(key + ": " + value));
+
+        jedis.close(); 
+    } 
+} 
 
 
 
