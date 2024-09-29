55075d1696c047badaae4f556b66b519a0898404
Joao <joaomonteiro29@ua.pt>
2024-09-29 22:50:04 +0100

    small

---
+++ Lab1/5/demo/src/main/java/com/app/Atendimento.java
@@ -0,0 +1,10 @@
+package com.app;
+
+public class Atendimento {
+    private final int products = 5;
+    private final int time = 1; // minutes
+    public static void main( String[] args ) {
+        Jedis jedis = new Jedis();
+        
+    }
+}
