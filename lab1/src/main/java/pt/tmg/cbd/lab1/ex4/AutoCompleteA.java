9e11a538f1fc6b0dc34f6bc02d6df8d0fe479877
Joao <joaomonteiro29@ua.pt>
2024-10-02 23:37:18 +0100

    done lab 1

---
+++ lab1/src/main/java/pt/tmg/cbd/lab1/ex4/AutoCompleteA.java
@@ -0,0 +1,35 @@
+package pt.tmg.cbd.lab1.ex4;
+
+import redis.clients.jedis.Jedis; 
+import java.util.Scanner;
+import java.io.File;
+
+public class AutoCompleteA {
+    public static String USERS = "user"; // Key set for users' name
+    public static void main( String[] args ) throws Exception {
+        Jedis jedis = new Jedis();
+
+        File file = new File("/home/joao/Desktop/3ºANO/1ºSEMESTRE/CBD/lab1/src/main/resources/names.txt");
+        Scanner reader = new Scanner(file);
+
+        while(reader.hasNextLine()) {
+            jedis.zadd(USERS, 0, reader.nextLine());
+        }
+
+        Scanner sc = new Scanner(System.in);
+        String name;
+        while (true) {
+            System.out.print("Search for ('Enter' for quit): ");
+            name = sc.nextLine().toLowerCase();
+            if (name.equals("")) {
+                break;
+            }
+
+            jedis.zrangeByLex(USERS, "[" + name, "(" + name + Character.MAX_VALUE).forEach(System.out::println);
+        }
+
+        reader.close();
+        sc.close();
+        jedis.close();
+    }
+}
---
+++ lab1/src/main/java/pt/tmg/cbd/lab1/ex4/AutoCompleteB.java
@@ -0,0 +1,43 @@
+package pt.tmg.cbd.lab1.ex4;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+
+public class AutoCompleteB {
+    public static String USERS_SCORE = "userByScore";
+
+    public static void main( String[] args ) throws Exception {
+        Jedis jedis = new Jedis();
+        jedis.flushDB();
+
+        File file = new File("/home/joao/Desktop/3ºANO/1ºSEMESTRE/CBD/lab1/src/main/resources/nomes-pt-2021.csv");
+        Scanner reader = new Scanner(file);
+
+        while(reader.hasNextLine()) {
+            String[] data = reader.nextLine().split(";");
+            jedis.zadd(USERS_SCORE, Double.parseDouble(data[1]), data[0].toLowerCase());
+        }
+
+        Scanner sc = new Scanner(System.in);
+        String name;
+        while (true) {
+            System.out.print("Search for ('Enter' for quit): ");
+            name = sc.nextLine().toLowerCase();
+            if (name.equals("")) {
+                break;
+            }
+
+            List<String> res = jedis.zrevrangeByScore(USERS_SCORE, "+inf", "-inf"); 
+            for (String n : res) {
+                if (n.startsWith(name)) System.out.println(n);
+            }
+        }
+
+        reader.close();
+        sc.close();
+        jedis.close();
+    }
+}
