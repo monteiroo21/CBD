bd25fefb0fba0924f6921391c03e2016eb133524
Joao <joaomonteiro29@ua.pt>
2024-10-24 15:51:58 +0100

    refactor later!

--- lab2/src/main/java/pt/tmg/cbd/lab2/ex3/d/Ex4a.java
+++ lab2/src/main/java/pt/tmg/cbd/lab2/ex3/d/Ex4a.java
@@ -1,17 +1,18 @@
 package pt.tmg.cbd.lab2.ex3.d;
 
 import java.util.Scanner;
-
+import java.util.Arrays;
+import java.util.List;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
-
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Sorts;
+import com.mongodb.client.model.Updates;
 
 public class Ex4a {
     private static final int products = 5;
@@ -19,25 +20,30 @@ public class Ex4a {
 
     public static void addRequest(String username, String product, Long timestamp, MongoCollection<Document> collection) {
         Bson filter = Filters.eq("user", username);
-        Long productCount = collection.countDocuments(filter); // update this piece of code if it no longer works
-        if (productCount == 0) {
+        Document userDoc = collection.find(filter).first();
+        if (userDoc == null) {
             collection.insertOne(new Document()
                 .append("_id", new ObjectId())
                 .append("user", username)
-                .append("requests", new Document()
+                .append("requests", Arrays.asList(new Document()
                         .append("product", product)
-                        .append("timestamp", timestamp)));
+                        .append("timestamp", timestamp))));
             System.out.println("Product '" + product + "' added for user '" + username + "'.");
-        } else if (productCount >= products) {
-            Long oldTimeStamp = getFirstTime(collection, filter);
-            System.out.println("ERROR: User " + username + " has exceeded the maximum number of requests in the current timeslot.");
-            System.out.println("You can make the next request in " + ((getTime() - oldTimeStamp) / 1000) + " seconds.");
         } else {
-            // I want to add a new product and the timestamp into the collection
-            System.out.println("Product '" + product + "' added for user '" + username + "'.");
-        }
-        if (((getTime() - getFirstTime(collection, filter)) / 1000) >= timeslot) {
-            // Remove first product and given timestamp
+            List<Document> requests = userDoc.getList("requests", Document.class);
+            if (requests.size() >= products) {
+                Long oldTimeStamp = getFirstTime(collection, filter);
+                System.out.println("ERROR: User " + username + " has exceeded the maximum number of requests in the current timeslot.");
+                System.out.println("You can make the next request in " + ((getTime() - oldTimeStamp) / 1000) + " seconds.");
+            } else {
+                collection.updateOne(filter, Updates.push("requests", new Document()
+                    .append("product", product)
+                    .append("timestamp", timestamp)));
+                System.out.println("Product '" + product + "' added for user '" + username + "'.");
+            }
+            if (((getTime() - getFirstTime(collection, filter)) / 1000) >= timeslot) {
+                collection.updateOne(filter, Updates.pullByFilter(Filters.eq("requests.timestamp", getFirstTime(collection, filter))));
+            }
         }
 
     }
@@ -45,7 +51,7 @@ public class Ex4a {
     public static Long getFirstTime(MongoCollection<Document> collection, Bson filter) {
         Bson sort = Sorts.ascending("timestamp");
         Document firstDocument = collection.find(filter).sort(sort).first();
-        return Long.parseLong(String.valueOf(firstDocument.get("timestamp")));
+        return Long.parseLong(String.valueOf(firstDocument.get("requests.timestamp")));
     }
 
     public static Long getTime() {
