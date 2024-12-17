8cad44487c6be5f3d9e64811e1a6b0a009343a46
Joao <joaomonteiro29@ua.pt>
2024-12-17 19:44:40 +0000

    start 4

--- lab4/src/main/java/pt/ua/cbd/lab4/ex4/Main.java
+++ lab4/src/main/java/pt/ua/cbd/lab4/ex4/Main.java
@@ -1,8 +1,16 @@
 package pt.ua.cbd.lab4.ex4;
 
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.Driver;
+
 public class Main {
-
-    public static void main(String... args) {
-
+    public static void main(String[] args) {
+        String address = "bolt://localhost:7687";
+        String user = "neo4j";
+        String password = "12345678";
+        Driver driver = GraphDatabase.driver(address, AuthTokens.basic(user, password));
+        Session session = driver.session();
     }
 }
