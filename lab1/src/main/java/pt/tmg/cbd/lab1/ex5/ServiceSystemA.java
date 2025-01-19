1d053d8dbeeed65df6f388a73e43c6c7c32a7571
Joao <joaomonteiro29@ua.pt>
2025-01-19 15:22:42 +0000

    .

--- lab1/src/main/java/pt/tmg/cbd/lab1/ex5/ServiceSystemA.java
+++ lab1/src/main/java/pt/tmg/cbd/lab1/ex5/ServiceSystemA.java
@@ -2,13 +2,11 @@ package pt.tmg.cbd.lab1.ex5;
 
 import redis.clients.jedis.Jedis;
 
-import java.util.Map;
 import java.util.Scanner;
 
 public class ServiceSystemA {
-    private static final String TIMESTAMPS = "timestamps";
     private static final int PRODUCTS = 5;
-    private static final int TIMESLOT = 30;
+    private static final int TIMESLOT = 30000;
     private static Jedis jedis;
 
     private static void addProduct(String username, String product) {
@@ -17,7 +15,10 @@ public class ServiceSystemA {
         jedis.zremrangeByScore(username, 0, cutoffTime);
         if (jedis.zcard(username) >= PRODUCTS) {
             System.out.println("ERROR: User " + username.substring(4) + " has exceeded the maximum number of requests in the current timeslot.");
-            Integer timeLeft = ((Integer.parseInt(jedis.hget(TIMESTAMPS, username)) + TIMESLOT) - time);
+            Integer timeLeft = (int) (jedis.zrangeWithScores(username, 0, 0).stream()
+                            .mapToDouble(tuple -> tuple.getScore())
+                            .min()
+                            .orElse((double) time) + TIMESLOT - time) / 1000;
             System.out.println("You can make the next request in " + timeLeft + " seconds.");
         } else {
             jedis.zadd(username, time, product);
@@ -29,20 +30,20 @@ public class ServiceSystemA {
         jedis = new Jedis();
         Scanner sc = new Scanner(System.in);
         while (true) {
-            System.out.println("Enter the username ('Enter' to quit): ");
+            System.out.print("Enter the username ('Enter' to quit): ");
             String username = sc.nextLine();
             if (username.isEmpty()) {
                 break;
             }
 
-            System.out.println("Enter the product ('Enter' to quit): ");
+            System.out.print("Enter the product ('Enter' to quit): ");
             String product = sc.nextLine();
             if (product.isEmpty()) {
                 break;
             }
 
-            username = "USER:" + username;
-            product = "PRODUCT:" + product;
+            username = "USER" + username;
+            product = "PRODUCT" + product;
             addProduct(username, product);
         }
         sc.close();
---
+++ lab1/src/main/java/pt/tmg/cbd/lab1/ex5/ServiceSystemB.java
@@ -0,0 +1,52 @@
+package pt.tmg.cbd.lab1.ex5;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.Scanner;
+
+public class ServiceSystemB {
+    private static final int QUANTITY = 30;
+    private static final int TIMESLOT = 30000;
+    private static Jedis jedis;
+
+    private static void addProduct(String username, String product) {
+        Integer time = (int) System.currentTimeMillis();
+        Integer cutoffTime = time - TIMESLOT;
+        jedis.zremrangeByScore(username, 0, cutoffTime);
+        if (jedis.zcard(username) >= PRODUCTS) {
+            System.out.println("ERROR: User " + username.substring(4) + " has exceeded the maximum number of requests in the current timeslot.");
+            Integer timeLeft = (int) (jedis.zrangeWithScores(username, 0, 0).stream()
+                            .mapToDouble(tuple -> tuple.getScore())
+                            .min()
+                            .orElse((double) time) + TIMESLOT - time) / 1000;
+            System.out.println("You can make the next request in " + timeLeft + " seconds.");
+        } else {
+            jedis.zadd(username, time, product);
+            System.out.println("Product '" + product.substring(7) + "' added for user '" + username.substring(4) + "'.");
+        }
+    }
+
+    public static void main( String[] args ) throws Exception {
+        jedis = new Jedis();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter the username ('Enter' to quit): ");
+            String username = sc.nextLine();
+            if (username.isEmpty()) {
+                break;
+            }
+
+            System.out.print("Enter the product ('Enter' to quit): ");
+            String product = sc.nextLine();
+            if (product.isEmpty()) {
+                break;
+            }
+
+            username = "USER" + username;
+            product = "PRODUCT" + product;
+            addProduct(username, product);
+        }
+        sc.close();
+        jedis.close();
+    }
+}
