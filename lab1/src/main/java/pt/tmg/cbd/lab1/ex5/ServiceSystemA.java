0086735f52f8f6dafb7e18b3f3ff8b79d74afcc9
Joao <joaomonteiro29@ua.pt>
2025-01-19 12:45:32 +0000

    .

--- lab1/src/main/java/pt/tmg/cbd/lab1/ex5/ServiceSystemA.java
+++ lab1/src/main/java/pt/tmg/cbd/lab1/ex5/ServiceSystemA.java
@@ -1,56 +1,51 @@
 package pt.tmg.cbd.lab1.ex5;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.Map;
 import java.util.Scanner;
-import redis.clients.jedis.Jedis; 
 
 public class ServiceSystemA {
-    private static final int products = 5;
-    private static final int timeslot = 30;
+    private static final String TIMESTAMPS = "timestamps";
+    private static final int PRODUCTS = 5;
+    private static final int TIMESLOT = 30;
+    private static Jedis jedis;
 
-    public static String TIMESTAMPS = "timestamps";
-
-    public static Jedis jedis;
-
-    public static void addRequest(String username, String product, Long timestamp) {
-        if (jedis.zcard(username) >= products) {
+    private static void addProduct(String username, String product) {
+        Integer time = (int) System.currentTimeMillis();
+        Integer cutoffTime = time - TIMESLOT;
+        jedis.zremrangeByScore(username, 0, cutoffTime);
+        if (jedis.zcard(username) >= PRODUCTS) {
             System.out.println("ERROR: User " + username.substring(4) + " has exceeded the maximum number of requests in the current timeslot.");
-            System.out.println("You can make the next request in " + jedis.ttl(username) + " seconds.");
-            
+            Integer timeLeft = ((Integer.parseInt(jedis.hget(TIMESTAMPS, username)) + TIMESLOT) - time);
+            System.out.println("You can make the next request in " + timeLeft + " seconds.");
         } else {
-            jedis.zadd(username, timestamp, product);
-            jedis.expire(username, timeslot);
-            jedis.hset(TIMESTAMPS, username, String.valueOf(timestamp)); 
+            jedis.zadd(username, time, product);
             System.out.println("Product '" + product.substring(7) + "' added for user '" + username.substring(4) + "'.");
         }
     }
 
-    public static String getTime() {
-        return jedis.time().get(0);
-    }
-
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws Exception {
         jedis = new Jedis();
         Scanner sc = new Scanner(System.in);
-        String username, product;
         while (true) {
-            System.out.print("Enter the username ('Enter' to quit): ");
-            username = sc.nextLine();
+            System.out.println("Enter the username ('Enter' to quit): ");
+            String username = sc.nextLine();
             if (username.isEmpty()) {
                 break;
             }
-            username = "USER" + username;
 
-            System.out.print("Enter the product ('Enter' to quit): ");
-            product = sc.nextLine();
+            System.out.println("Enter the product ('Enter' to quit): ");
+            String product = sc.nextLine();
             if (product.isEmpty()) {
                 break;
             }
-            product = "PRODUCT" + product;
 
-            Long timestamp = Long.parseLong(getTime());
-            addRequest(username, product, timestamp);
+            username = "USER:" + username;
+            product = "PRODUCT:" + product;
+            addProduct(username, product);
         }
         sc.close();
         jedis.close();
     }
 }
-
