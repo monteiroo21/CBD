9bd19150095e2442483d00ffcf2d134fc55c9faf
Joao <joaomonteiro29@ua.pt>
2025-01-20 16:33:22 +0000

    .

--- lab2/resources/populatePhones.js
+++ lab2/resources/populatePhones.js
@@ -1,19 +1,24 @@
-numPhones = function () {
-  var listPhones = db.phones.aggregate([
-      {
-          $group: {
-              _id: "$components.prefix",
-              numPhones: { $sum: 1 }
-          }
-      },
-      {
-          $project: {
-              _id: 0, 
-              prefix: '$_id',
-              numPhones: 1 
-          }
-      }
-    ])
-  
-  return result;
-}
+function populatePhones(country, start, stop) { 
+ 
+    var prefixes = [21, 22, 231, 232, 233, 234 ]; 
+    for (var i = start; i <= stop; i++) { 
+   
+      var prefix = prefixes[(Math.random() * 6) << 0] 
+      var countryNumber =  
+      (prefix * Math.pow(10, 9 - prefix.toString().length)) + i; 
+      var num = (country * 1e9) + countryNumber; 
+      var fullNumber = "+" + country + "-" + countryNumber; 
+   
+      db.phones.insert({ 
+        _id: num, 
+        components: { 
+          country: country, 
+          prefix: prefix, 
+          number: i 
+        }, 
+        display: fullNumber 
+      }); 
+      print("Inserted number " + fullNumber); 
+    } 
+    print("Done!"); 
+  } 
